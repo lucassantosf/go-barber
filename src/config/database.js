7faeb5c8c040fe8ca74f7e892e957599e0bf94e0
lucassantosf <lucas46em@gmail.com>
2020-05-20 18:19:11 -0300

    Refact structure folders and first migration and model user

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  port: '3308',
+  username: 'root',
+  password: '',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
