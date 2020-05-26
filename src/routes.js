089a909e627c158b771a4198b23fb67ff74e6a90
lucassantosf <lucas46em@gmail.com>
2020-05-26 18:24:51 -0300

    Using multer for storing files

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 const authMiddleware = require('./app/middlewares/auth');
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -11,5 +15,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 module.exports = routes;
