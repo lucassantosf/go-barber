2cac2906900b3aec5299bdd77d061c38e6aacb4f
lucassantosf <lucas46em@gmail.com>
2020-06-12 17:22:29 -0300

    Studying react

---
+++ react/src/App.js
@@ -0,0 +1,10 @@
+const React = require('react');
+require('./App.css');
+const profile = require('./assets/ico.jpg');
+const TechList = require('./components/TechList');
+
+function App() {
+  return <TechList />;
+}
+
+module.exports = App;
