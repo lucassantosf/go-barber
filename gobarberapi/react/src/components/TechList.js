91795c983b8bbca77847e3f6d07296bcad7586cb
lucassantosf <lucas46em@gmail.com>
2020-07-22 18:31:29 -0300

    Redifine project structure on api and web, init react app for web

---
+++ gobarberapi/react/src/components/TechList.js
@@ -0,0 +1,61 @@
+const React = require('react');
+const { Component } = require('react');
+const TechItem = require('./TechItem');
+
+class TechList extends Component {
+  static defaultProps = {
+    tech: 'Teste',
+  };
+
+  state = {
+    newTech: '',
+    techs: [],
+  };
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+  handleInputChange = (e) => {
+    this.setState({ newTech: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+    });
+  };
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter((t) => t !== tech) });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map((tech) => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Salvar</button>
+      </form>
+    );
+  }
+}
+
+module.exports = TechList;
---
+++ gobarberweb/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <h1>Gobarber web</h1>;
+}
+
+export default App;
