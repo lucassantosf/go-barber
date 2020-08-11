aafa02a0b433eb7567438a40770b34164e4e47fd
lucassantosf <lucas46em@gmail.com>
2020-08-11 18:54:15 -0300

    Finished gobarberweb

--- gobarberweb/src/components/Header/index.js
+++ gobarberweb/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import Notifications from "~/components/Notifications";
@@ -7,6 +8,7 @@ import logo from "~/assets/logo.png";
 import { Container, Content, Profile } from "./styles";
 
 export default function Header() {
+  const profile = useSelector((state) => state.user.profle);
   return (
     <Container>
       <Content>
@@ -18,12 +20,15 @@ export default function Header() {
           <Profile>
             <Notifications />
             <div>
-              <strong>Lucas Santos</strong>
+              <strong>{profile && profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Lucas Santos"
+              src={
+                (profile && profile.avatar.url) ||
+                "https://api.adorable.io/avatars/50/abott@adorable.png"
+              }
+              alt={(profile && profile.name) || "Avatar"}
             />
           </Profile>
         </aside>
