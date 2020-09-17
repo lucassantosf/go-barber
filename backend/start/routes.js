75667cc8a6c5e9f4974365e8ff4474a1e563af64
lucassantosf <lucas46em@gmail.com>
2020-09-17 17:34:39 -0300

    Initialized a new api with adonisjs

---
+++ backend/start/routes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use("Route");
+
+Route.post("users", "UserController.store");
+Route.post("sessions", "SessionController.store");
+
+Route.group(() => {
+  Route.resource("teams", "TeamController").apiOnly();
+}).middleware("auth");
+
+Route.group(() => {
+  Route.post("invites", "InviteController.store");
+}).middleware(["auth", "team"]);
